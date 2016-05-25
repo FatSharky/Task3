56210a7e4ee3b446353ceb3e96479c7f142d7f6c
Sharkai <vladislavbypinsk@mail.ru>
2016-05-25 12:01:58 +0300

    add static method FoodTagName

--- src/by/traning/task3/domain/FoodTagName.java
+++ src/by/traning/task3/domain/FoodTagName.java
@@ -2,6 +2,37 @@ package by.traning.task3.domain;
 
 public enum FoodTagName {
 
-	FOOD, TYPE, PHOTO, NAME, DESCRIPTION, PORTION, PRICE, MENU
+	FOOD, TYPE, PHOTO, NAME, DESCRIPTION, PORTION, PRICE, MENU;
+	
+	public static FoodTagName getElementTagName(String element){
+		switch (element) {
+		case "food":
+			return FOOD;
+			
+		case "type":
+			return TYPE;
+			
+		case "name":
+			return NAME;
+			
+		case "photo":
+			return PHOTO;
+			
+		case "description":
+			return DESCRIPTION;
+	
+		case "portion":
+			return PORTION;
+			
+		case "price":
+			return PRICE;
+			
 
+		default: throw new EnumConstantNotPresentException(FoodTagName.class, element);
+		
+		
+		}
+	}
+
+	
 }
